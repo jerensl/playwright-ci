990a76372e8ceca8ecd8ab0fc12d79757994cdd8
jerensl <54782057+jerensl@users.noreply.github.com>
2025-03-03 21:30:29 +0800

    feat: add structure logging with date and color
    Signed-off-by: jerensl <54782057+jerensl@users.noreply.github.com>

--- packages/playwright-reporter/src/logging.ts
+++ packages/playwright-reporter/src/logging.ts
@@ -1,21 +1,11 @@
-export interface ILogger {
-        readonly green: string;
-        readonly red: string;
-        readonly yellow: string;
-        readonly blue: string;
-        readonly reset: string;
-        info(message: string): void;
-        warn(message: string): void;
-        debug(message: string): void;
-        success(message: string): void;
-        error(message: string): void;
-}
+import dayjs, { Dayjs } from "dayjs"
 
-export class MultiLineLogger implements ILogger {
+export abstract class Logger {
         readonly green: string;
         readonly red: string;
         readonly yellow: string;
         readonly blue: string;
+        readonly purple: string;
         readonly reset: string;
 
         constructor() {
@@ -23,65 +13,100 @@ export class MultiLineLogger implements ILogger {
                 this.red = "\x1b[31m"        // Red
                 this.yellow = "\x1b[33m"     // Yellow
                 this.blue = "\x1b[34m"       // Blue
+                this.purple = "\x1b[35m"     // Purple
                 this.reset = "\x1b[0m"       // Reset color
         }
 
+        getTime(currentDate: Dayjs) {
+                return dayjs(currentDate).format("YYYY-MM-DD HH-mm-ss")
+        }
+        raw(message: string): void { };
+        info(message: string): void { };
+        test(project: string, message: string): void { };
+        warn(message: string): void { };
+        debug(message: string): void { };
+        success(project: string, message: string): void { };
+        error(project: string, message: string): void { };
+}
+
+
+export class MultiLineLogger extends Logger {
+        constructor() {
+                super()
+        }
+
+        raw(message: string): void {
+                process.stdout.write(`${this.getTime(dayjs())} ${message}\n`)
+        }
         info(message: string): void {
-                process.stdout.write(`${this.blue}Info: ${this.reset}${message}\n`)
+                process.stdout.write(`${this.getTime(dayjs())}${this.blue} [Info]: ${this.reset}${message}\n`)
+        }
+        test(project: string, message: string): void {
+                process.stdout.write(`${this.getTime(dayjs())}${this.blue} [Test]:${this.reset} ${this.purple}[${project}]${this.reset} ${message}\n`)
         }
         warn(message: string): void {
-                process.stdout.write(`${this.yellow}Warn: ${this.reset}${message}\n`)
+                process.stdout.write(`${this.getTime(dayjs())}${this.yellow} [Warn]: ${this.reset}${message}\n`)
         }
         debug(message: string): void {
-                process.stdout.write(`${this.yellow}Debug: ${this.reset}${message}\n`)
+                process.stdout.write(`${this.getTime(dayjs())}${this.yellow} [Debug]: ${this.reset}${message}\n`)
         }
-        success(message: string): void {
-                process.stdout.write(`${this.green}Success: ${this.reset}${message}\n`)
+        success(project: string, message: string): void {
+                process.stdout.write(`${this.getTime(dayjs())}${this.green} [Success]:${this.reset} ${this.purple}[${project}]${this.reset} ${message}\n`)
         }
-        error(message: string): void {
-                process.stdout.write(`${this.red}Error: ${this.reset}${message}\n`)
+        error(project: string, message: string): void {
+                process.stdout.write(`${this.getTime(dayjs())}${this.red} [Error]:${this.reset} ${this.purple}[${project}]${this.reset} ${message}\n`)
         }
 }
 
-export class SingleLineLogger implements ILogger {
-        readonly green: string;
-        readonly red: string;
-        readonly yellow: string;
-        readonly blue: string;
-        readonly reset: string;
-
+export class SingleLineLogger extends Logger {
         constructor() {
-                this.green = "\x1b[32m"      // Green
-                this.red = "\x1b[31m"        // Red
-                this.yellow = "\x1b[33m"     // Yellow
-                this.blue = "\x1b[34m"       // Blue
-                this.reset = "\x1b[0m"       // Reset color
+                super()
         }
 
+        raw(message: string): void {
+                process.stdout.write(`${this.getTime(dayjs())} ${message}\n\n`)
+        }
         info(message: string): void {
+                process.stdout.moveCursor(0, -1)
                 process.stdout.clearLine(0)
-                process.stdout.write(`${this.blue}Info: ${this.reset}${message}\n`)
+                process.stdout.cursorTo(0)
+                process.stdout.write(`${this.getTime(dayjs())}${this.blue} [Info]: ${this.reset}${message}\n`)
+        }
+        test(project: string, message: string): void {
+                process.stdout.moveCursor(0, -1)
+                process.stdout.clearLine(0)
+                process.stdout.cursorTo(0)
+                process.stdout.write(`${this.getTime(dayjs())}${this.blue} [Test]:${this.reset} ${this.purple}[${project}]${this.reset} ${message}\n`)
         }
         warn(message: string): void {
+                process.stdout.moveCursor(0, -1)
                 process.stdout.clearLine(0)
-                process.stdout.write(`${this.yellow}Warn: ${this.reset}${message}\n`)
+                process.stdout.cursorTo(0)
+                process.stdout.write(`${this.getTime(dayjs())}${this.yellow} [Warn]: ${this.reset}${message}\n`)
         }
         debug(message: string): void {
+                process.stdout.moveCursor(0, -1)
                 process.stdout.clearLine(0)
-                process.stdout.write(`${this.yellow}Debug: ${this.reset}${message}\n`)
+                process.stdout.cursorTo(0)
+                process.stdout.write(`${this.getTime(dayjs())}${this.yellow} [Debug]: ${this.reset}${message}\n`)
         }
-        success(message: string): void {
+        success(project: string, message: string): void {
+                process.stdout.moveCursor(0, -1)
                 process.stdout.clearLine(0)
-                process.stdout.write(`${this.green}Success: ${this.reset}${message}\n`)
+                process.stdout.cursorTo(0)
+                process.stdout.write(`${this.getTime(dayjs())}${this.green} [Success]:${this.reset} ${this.purple}[${project}]${this.reset} ${message}\n`)
+
         }
-        error(message: string): void {
+        error(project: string, message: string): void {
+                process.stdout.moveCursor(0, -1)
                 process.stdout.clearLine(0)
-                process.stdout.write(`${this.red}Error: ${this.reset}${message}\n`)
+                process.stdout.cursorTo(0)
+                process.stdout.write(`${this.getTime(dayjs())}${this.red} [Error]:${this.reset} ${this.purple}[${project}]${this.reset} ${message}\n`)
         }
 }
 
 export default class LoggerFactory {
-        public static createLogger(type: string): ILogger {
+        public static createLogger(type: string): Logger {
                 if (type === "single") {
                         return new SingleLineLogger()
                 } else {
